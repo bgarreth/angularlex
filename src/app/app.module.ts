7af811b8d2eb6d17e7eae0bc6e953038299ea73f
EC2 Default User <ec2-user@ip-172-31-31-19.eu-west-1.compute.internal>
2021-06-28 09:07:17 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import {AmplifyUIAngularModule} from "@aws-amplify/ui-angular";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [AmplifyUIAngularModule, BrowserModule,AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Amplify from 'aws-amplify';
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'eu-west-1:0e7301d8-bb37-4de7-afaf-5d5e6708c454',
+    region: 'eu-west-1'
+  },
+  Interactions: {
+    bots: {
+      "BookTrip_dev": {
+        "name": "BookTrip_dev",
+        "alias": "$LATEST",
+        "region": "eu-west-1",
+      },
+    }
+  }
+});
+
+
 if (environment.production) {
   enableProdMode();
 }
